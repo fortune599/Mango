72d2d87bef986a55e0de621855dcb4c32935111c
James Wang <jameswang9909@hotmail.com>
2017-02-07 13:24:28 -0500

    Add teachers collection

--- db_builder.py
+++ db_builder.py
@@ -6,6 +6,7 @@ db = client["mango"]
 
 csv_courses = csv.DictReader(open("courses.csv"))
 csv_students = csv.DictReader(open("peeps.csv"))
+csv_teachers = csv.DictReader(open("teachers.csv"))
 
 students = {}
 for row in csv_students:
@@ -25,3 +26,21 @@ for row in csv_courses:
         })
 
 db.students.insert_many(students.values())
+
+teachers = {}
+for row in csv_teachers:
+    code = row["code"]
+    name = row["teacher"]
+    period = row["period"]
+    teachers[code] = {
+        "code": code,
+        "name": name,
+        "period": int(period),
+        "students": []
+    }
+
+csv_courses = csv.DictReader(open("courses.csv"))
+for row in csv_courses:
+    teachers[row["code"]]["students"].append(int(row["id"]))
+
+db.teachers.insert_many(teachers.values())
